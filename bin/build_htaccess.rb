90922c06b8649711aa00a8dffc3c5dd8ec7b7464
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-03-30 01:46:41 +0900

    Change: Added path to lib directory.

--- bin/build_htaccess.rb
+++ bin/build_htaccess.rb
@@ -1,3 +1,4 @@
+$: << File.expand_path(File.dirname(__FILE__)) + "/../lib"
 require 'htaccess_dsl'
 
 include HtaccessDsl
--- example/build_example.rb
+++
@@ -1,3 +0,0 @@
-$: << File.dirname(File.expand_path(__FILE__)) + "/../lib/"
-$: << File.dirname(File.expand_path(__FILE__)) + "/../bin/"
-require 'build_htaccess'
