198c1534ab58811c67e49749f95c328f42e5a627
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-03-30 01:05:20 +0900

    Change: Coding style.

--- spec/htaccess_spec.rb
+++ spec/htaccess_spec.rb
@@ -4,18 +4,22 @@ include HtaccessDsl
 
 describe Htaccess do
   context 'DSL given { Foo "Bar Baz" }' do
-    subject { dsl { Foo "Bar Baz" } }
+    subject do
+      dsl {
+        Foo "Bar Baz"
+      }
+    end
 
     its(:to_ary) { should == ["Foo Bar Baz"] }
   end
 
   context 'DSL given { Foo "Bar Baz"; Hoge "Fuga Piyo" }' do
-    subject {
+    subject do
       dsl {
         Foo  "Bar Baz"
         Hoge "Fuga Piyo"
       }
-    }
+    end
 
     its(:to_ary) { should == ["Foo Bar Baz", "Hoge Fuga Piyo"] }
   end
